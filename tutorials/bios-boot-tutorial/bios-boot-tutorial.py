4cfc7a3b5e3bca9d7b8fc4bd12c71ca44b8f0cd4
Baixiang Liu <bxliu@fudan.edu.cn>
2018-10-22 07:17:37 +0800

    add hash part for create proposal test script

--- tutorials/bios-boot-tutorial/bios-boot-tutorial.py
+++ tutorials/bios-boot-tutorial/bios-boot-tutorial.py
@@ -212,7 +212,7 @@ def createGocStakedAccounts(b, e):
 def createGocProposals(b, e):
     for i in range(b, e):
         a = accounts[i]
-        run(args.cleos + 'system createproposal %s "%s" a%s b%s c%s --start-type %s' % (a['name'], intToCurrency(10000000), i, i, i, i%3))
+        run(args.cleos + 'system createproposal %s "%s" a%s b%s c%s d%s --start-type %s' % (a['name'], intToCurrency(10000000), i, i, i, i, i%3))
 
 def createGocVotes(b):
     for i in range(0, b):
